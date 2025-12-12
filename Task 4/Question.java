7078a328fb865d40b252f515880a79501f3b10dd
Arsema137 <arsemaseife137@gmail.com>
2025-12-11 22:44:58 -0800

    Add Question class for quiz functionality

---
+++ Task 4/Question.java
@@ -0,0 +1,22 @@
+public class Question {
+    String question;
+    String[] options;
+    int correctOption;
+
+    public Question(String question, String[] options, int correctOption) {
+        this.question = question;
+        this.options = options;
+        this.correctOption = correctOption;
+    }
+
+    public void display() {
+        System.out.println(question);
+        for (int i = 0; i < options.length; i++) {
+            System.out.println((i + 1) + ". " + options[i]);
+        }
+    }
+
+    public boolean checkAnswer(int ans) {
+        return ans == correctOption;
+    }
+}
